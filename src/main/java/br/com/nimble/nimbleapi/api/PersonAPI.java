770467ab72ee43b301c95863fb2fcb2f4ddb264f
tiago002pro <tiagobarbosa02@outlook.com>
2021-07-30 14:15:58 -0300

    Ambiente preparado

---
+++ src/main/java/br/com/nimble/nimbleapi/api/PersonAPI.java
@@ -0,0 +1,28 @@
+package br.com.nimble.nimbleapi.api;
+
+import br.com.nimble.nimbleapi.model.Person;
+import br.com.nimble.nimbleapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PersonAPI {
+    @Autowired
+    PersonService service;
+
+    @PostMapping("/cadastro")
+    public String setPerson(Map<String, String> json) {
+        return this.service.createPerson(json);
+    }
+
+    @GetMapping("/getList")
+    public List<Person> gerListPerson() {
+        return this.service.getLisPerson();
+    }
+}
+
---
+++ src/main/java/br/com/nimble/nimbleapi/model/Person.java
@@ -0,0 +1,11 @@
+package br.com.nimble.nimbleapi.model;
+
+public class Person {
+    private String name;
+    private String age;
+
+    public Person(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+}
