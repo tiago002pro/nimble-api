299e0a41c507e47ebb5c9d99e80694a859c5fe02
Tiago <tiagobarbosa02@outlook.com>
2021-07-29 22:14:46 -0300

    First commit

---
+++ src/main/java/br/com/nimble/nimbleapi/NimbleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nimble.nimbleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbleApiApplication.class, args);
+	}
+
+}
