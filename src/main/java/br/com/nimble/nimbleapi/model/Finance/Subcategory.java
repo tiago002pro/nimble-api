687e432ce5abb9513a209786f2b4c2486f209ba2
Tiago <tiagobarbosa02@outlook.com>
2021-09-20 07:06:20 -0300

    Criando o financeiro

---
+++ src/main/java/br/com/nimble/nimbleapi/model/Finance/Subcategory.java
@@ -0,0 +1,27 @@
+package br.com.nimble.nimbleapi.model.Finance;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "subcategory")
+public class Subcategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
