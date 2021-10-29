2cf625713b68669e96476ba541ea3fe5e7d36574
Tiago barbosa <tiagobarbosa02@outlook.com>
2021-10-29 17:37:13 -0300

    Tela de login

--- src/main/java/br/com/nimble/nimbleapi/config/ConfigureGlobal.java
+++ src/main/java/br/com/nimble/nimbleapi/config/ConfigureGlobal.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ConfigureGlobal extends WebSecurityConfigurerAdapter {
+    //Login
     @Autowired
     NimbleUserService service;
 
