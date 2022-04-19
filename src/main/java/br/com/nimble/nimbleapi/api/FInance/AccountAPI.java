fb76a04fdeb181bedbac918a0f2c8e36539d6003
Tiago barbosa <tiagobarbosa02@outlook.com>
2022-04-19 13:44:56 -0300

    get all accounts

--- src/main/java/br/com/nimble/nimbleapi/api/FInance/AccountAPI.java
+++ src/main/java/br/com/nimble/nimbleapi/api/FInance/AccountAPI.java
@@ -3,6 +3,7 @@ package br.com.nimble.nimbleapi.api.FInance;
 import br.com.nimble.nimbleapi.model.Finance.Account;
 import br.com.nimble.nimbleapi.service.Finance.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +17,16 @@ public class AccountAPI {
     @CrossOrigin
     @Transactional
     @RequestMapping(method = RequestMethod.POST, value = "/form")
-    public ResponseEntity createTitle(@RequestBody Account resource)  {
+    public ResponseEntity createAccount(@RequestBody Account resource)  {
         return ResponseEntity.ok(this.service.createAccount(resource));
+    }
+
+    @CrossOrigin
+    @Transactional
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
+    public ResponseEntity getAllAccounts(@RequestParam(value = "type") String type,
+                                          @RequestParam(defaultValue = "1", required = false) int page,
+                                          @RequestParam(defaultValue = "10", required = false) int size) {
+        return ResponseEntity.ok(this.service.getAllAccounts(PageRequest.of(page > 0 ? --page : 0, size)));
     }
 }
--- src/main/java/br/com/nimble/nimbleapi/service/Finance/AccountService.java
+++ src/main/java/br/com/nimble/nimbleapi/service/Finance/AccountService.java
@@ -1,8 +1,11 @@
 package br.com.nimble.nimbleapi.service.Finance;
 
 import br.com.nimble.nimbleapi.model.Finance.Account;
+import br.com.nimble.nimbleapi.model.Finance.Title;
 import br.com.nimble.nimbleapi.repository.Finance.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +15,9 @@ public class AccountService {
 
     public Account createAccount(Account resource) {
         return this.repository.save(resource);
+    }
+
+    public Page<Account> getAllAccounts(Pageable pageable) {
+        return this.repository.findAll(pageable);
     }
 }
