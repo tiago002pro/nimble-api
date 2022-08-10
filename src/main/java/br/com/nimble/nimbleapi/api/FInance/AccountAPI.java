e1ac4f7728f48c08339fb383d87ab7b58ba21509
Tiago barbosa <tiagobarbosa02@outlook.com>
2022-08-09 23:22:23 -0300

    Clean code no modulo finance-account

--- src/main/java/br/com/nimble/nimbleapi/api/FInance/AccountAPI.java
+++ src/main/java/br/com/nimble/nimbleapi/api/FInance/AccountAPI.java
@@ -15,24 +15,23 @@ public class AccountAPI {
     AccountService service;
 
     @CrossOrigin
-    @Transactional
-    @RequestMapping(method = RequestMethod.POST, value = "/form")
+    @RequestMapping(method = RequestMethod.POST, value = "/new")
     public ResponseEntity createAccount(@RequestBody Account resource)  {
         return ResponseEntity.ok(this.service.createAccount(resource));
     }
 
     @CrossOrigin
-    @Transactional
+    @RequestMapping(method = RequestMethod.GET, value = "/all")
+    public ResponseEntity getAllAccounts(@RequestParam(defaultValue = "1", required = false) int page,
+                                         @RequestParam(defaultValue = "10", required = false) int size) {
+        return ResponseEntity.ok(this.service.getAllAccounts(PageRequest.of(page > 0 ? --page : 0, size)));
+    }
+
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public ResponseEntity getByid(@PathVariable Long id)  {
         return ResponseEntity.ok(this.service.getById(id));
     }
 
-    @CrossOrigin
-    @Transactional
-    @RequestMapping(method = RequestMethod.GET, value = "/list")
-    public ResponseEntity getAllAccounts(@RequestParam(defaultValue = "1", required = false) int page,
-                                         @RequestParam(defaultValue = "10", required = false) int size) {
-        return ResponseEntity.ok(this.service.getAllAccounts(PageRequest.of(page > 0 ? --page : 0, size)));
-    }
+
 }
--- src/main/java/br/com/nimble/nimbleapi/service/Finance/AccountService.java
+++ src/main/java/br/com/nimble/nimbleapi/service/Finance/AccountService.java
@@ -7,20 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AccountService {
     @Autowired
     AccountRepository repository;
 
-    public Account createAccount(Account resource) {
-        return this.repository.save(resource);
+    @Transactional
+    public Account createAccount(Account account) {
+        return this.repository.save(account);
     }
 
+    @Transactional
     public Page<Account> getAllAccounts(Pageable pageable) {
         return this.repository.findAll(pageable);
     }
 
+    @Transactional
     public Account getById(Long id) {
         return this.repository.findById(id).get();
     }
